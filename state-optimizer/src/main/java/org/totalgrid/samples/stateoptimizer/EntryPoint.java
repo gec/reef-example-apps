fa0c9886da85f574e718f2d88bc563e360572f1b
devans <devans@greenenergycorp.com>
2011-08-04 16:14:10 -0400

    Example app loads from Reef config file

--- state-optimizer/src/main/java/org/totalgrid/samples/stateoptimizer/EntryPoint.java
+++ state-optimizer/src/main/java/org/totalgrid/samples/stateoptimizer/EntryPoint.java
@@ -11,13 +11,25 @@ import org.totalgrid.reef.japi.client.SessionExecutionPool;
 import org.totalgrid.reef.messaging.javaclient.AMQPConnection;
 import org.totalgrid.reef.proto.ReefServicesList;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * logs into reef, initializes and authorizes a session and passes it to the StateOptimizerManager
  */
 public class EntryPoint {
 
     public static void main(String[] args) {
-        AMQPConnectionSettings info = getConnectionInfo();
+        AMQPConnectionSettings info = null;
+        if (args.length == 0 ) {
+            info = getConnectionInfo("org.totalgrid.reef.amqp.cfg");
+        } else if (args.length == 1) {
+            info = getConnectionInfo(args[0]);
+        } else {
+            System.out.println("Error: too many arguments.");
+            System.exit(-1);
+        }
 
         // configure the connection with list of services and address
         Connection connection = new AMQPConnection(info, ReefServicesList.getInstance(), 5000);
@@ -53,23 +65,40 @@ public class EntryPoint {
     }
 
     /**
-     * gets the ip of the reef server, defaults to 127.0.0.1 but can be override with java property
-     *  -Dorg.totalgrid.reef.amqp.host=192.168.100.10
+     * Loads broker configuration from Reef settings file
      * @return settings to connect to the broker
      */
-    private static AMQPConnectionSettings getConnectionInfo() {
-        String reef_ip = System.getProperty("org.totalgrid.reef.amqp.host");
-        if (reef_ip == null) reef_ip = "127.0.0.1";
-        String reef_port = System.getProperty("org.totalgrid.reef.amqp.port");
-        if (reef_port == null) reef_port = "5672";
-        String user = System.getProperty("org.totalgrid.reef.amqp.user");
-        if (user == null) user = "guest";
-        String password = System.getProperty("org.totalgrid.reef.amqp.password");
-        if (password == null) password = "guest";
-        String virtualHost = System.getProperty("org.totalgrid.reef.amqp.virtualHost");
-        if (virtualHost == null) virtualHost = "test";
+    private static AMQPConnectionSettings getConnectionInfo(String configFile) {
+        Properties props = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream(configFile);
+            props.load(fis);
+            fis.close();
+        } catch (IOException ex) {
+            System.out.println("Could not load config file: " + ex.toString());
+            System.exit(-1);
+        }
 
-        return new AMQPConnectionSettings(reef_ip, Integer.parseInt(reef_port), user, password, virtualHost);
+        String reefIp = loadProperty("org.totalgrid.reef.amqp.host", props);
+        String port = loadProperty("org.totalgrid.reef.amqp.port", props);
+        String user = loadProperty("org.totalgrid.reef.amqp.user", props);
+        String password = loadProperty("org.totalgrid.reef.amqp.password", props);
+        String virtualHost = loadProperty("org.totalgrid.reef.amqp.virtualHost", props);
+
+        return new AMQPConnectionSettings(reefIp, Integer.parseInt(port), user, password, virtualHost);
+    }
+
+    private static String loadProperty(String id, Properties props) {
+        String prop = props.getProperty(id);
+        if (prop == null) {
+            loadFailure(id);
+        }
+        return prop;
+    }
+
+    private static void loadFailure(String missing) {
+        System.out.println("Could not load configuration. Missing: " + missing);
+        System.exit(-1);
     }
 
 }
