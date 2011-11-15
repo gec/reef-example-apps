772f75003354ebcf904c82535ad3a6a22140c7bc
devans <devans@greenenergycorp.com>
2011-11-15 13:13:27 -0500

    Commented the entry points

--- commands/src/main/java/org/totalgrid/reef/examples/commands/CommandsExample.java
+++ commands/src/main/java/org/totalgrid/reef/examples/commands/CommandsExample.java
@@ -222,23 +222,46 @@ public class CommandsExample {
     }
 
 
+    /**
+     * Java entry-point for running examples.
+     *
+     * Starts a client connection to Reef, logs in, and executes example code.
+     *
+     * @param args Command line arguments
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
+
+        // Parse command line arguments
         if (args.length < 2) {
             System.out.println("Usage: <broker settings> <user settings>");
             System.exit(-1);
         }
 
         int result = 0;
+
+        // Load broker settings from config file
         AmqpSettings amqp = new AmqpSettings(args[0]);
+
+        // Load user settings (login credentials) from config file
         UserSettings user = new UserSettings(args[1]);
 
+        // Create a ConnectionFactory by passing the broker settings. The ConnectionFactory is
+        // used to create a Connection to the Reef server
         ConnectionFactory connectionFactory = new ReefConnectionFactory(amqp);
+
+        // Prepare a Connection reference so it can be cleaned up in case of an error
         Connection connection = null;
 
         try {
 
+            // Connect to the Reef server, may fail if can't connect
             connection = connectionFactory.connect();
+
+            // Login with the user credentials
             Client client = connection.login(user);
+
+            // Run examples...
 
             getCommands(client);
 
@@ -254,14 +277,20 @@ public class CommandsExample {
 
         } catch(ReefServiceException rse) {
 
+            // Handle ReefServiceException, potentially caused by connection, login, or service request errors
             System.out.println("Reef service error: " + rse.getMessage() + ". check that Reef server is running.");
             rse.printStackTrace();
             result = -1;
 
         } finally {
+
             if(connection != null) {
+
+                // Disconnect the Connection object, removes clients and subscriptions
                 connection.disconnect();
             }
+
+            // Terminate the ConnectionFactory to close threading objects
             connectionFactory.terminate();
         }
 
